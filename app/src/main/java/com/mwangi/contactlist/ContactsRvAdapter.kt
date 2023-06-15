b8a1d9cc2883f85d25981bca851744b43aca931c
Esther <mwangiestherwambuii@gmail.com>
2023-06-15 14:32:12 +0300

    Added a floating add contact button

--- app/src/main/java/com/mwangi/contactlist/ContactsRvAdapter.kt
+++ app/src/main/java/com/mwangi/contactlist/ContactsRvAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mwangi.contactlist.databinding.ActivityMainBinding
 import com.mwangi.contactlist.databinding.ContactsListItemBinding
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 
 class ContactsRvAdapter(var contactList:List<ContactsData>):RecyclerView.Adapter<ContactsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
@@ -17,6 +19,13 @@ class ContactsRvAdapter(var contactList:List<ContactsData>):RecyclerView.Adapter
         holder.binding.tvName.text = contact.name
         holder.binding.tvEmailAddress.text = contact.emailAddress
         holder.binding.tvPhoneNumber.text = contact.phoneNumber.toString()
+        Picasso
+            .get()
+            .load(contact.avatar)
+            .resize(80,80)
+            .centerCrop()
+            .transform(CropCircleTransformation())
+            .into(holder.binding.ivContactPhoto)
     }
 
     override fun getItemCount(): Int {
